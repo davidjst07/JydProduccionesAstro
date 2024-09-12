f55c388361b3cc3f0428f6bb19a4758e6e0b20fb
davidjst07 <davidjst@hotmail.com>
2024-09-12 15:42:08 -0500

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
-import compress from 'astro-compress'
-import icon from "astro-icon"
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  integrations: [mdx(), icon(), tailwind({
-    applyBaseStyles: false,
-  }), compress()],
-})
+  integrations: [ // Usa el nombre del plugin correctamente
+    mdx(),
+    icon(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    compress(),
+  ],
+});
